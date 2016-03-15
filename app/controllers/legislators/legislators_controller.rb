2d1d9b56f970309123f232773a7ed2bcb017ce41
Taylor Moore <Tmoore2272@gmail.com>
2016-03-15 10:34:30 -0600

    Gets news from GoogleService

--- app/controllers/legislators/legislators_controller.rb
+++ app/controllers/legislators/legislators_controller.rb
@@ -2,6 +2,9 @@ class Legislators::LegislatorsController < ApplicationController
 
   def show
     @legislator = Legislator.find(params[:id])
+    @news = GoogleService.new(@legislator.name)
+    @news = @news.get_news
+    binding.pry
   end
 
 end
--- app/services/google_service.rb
+++ app/services/google_service.rb
@@ -2,15 +2,18 @@ class GoogleService
 
   attr_reader :connection
 
-  def initialize()
-    @connection =
-    Faraday.new('https://ajax.googleapis.com/ajax/services/search/news?v=1.0&q=barack%20obama')
+  def initialize(search)
+    @connection = Faraday.new(:url => 'https://ajax.googleapis.com/ajax/services/search/news?v=1.0&q=barack%20obama', :ssl => {verify: false})
+    connection.params = { "v" => "1.0", "q" => search}
   end
 
+  def get_news
+    parse(connection.get)
+  end
 
   private
 
   def parse(response)
-    JSON.parse(response.body)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
